c0d9aed06a26a1aa227a4e7127553dffb7eb166a
esteban <relbigote75@gmail.com>
2025-11-23 23:46:27 -0300

    refactor: added arg attribute launch.json

--- Data_Structures/Use-Array1.1.ps1
+++ Data_Structures/Use-Array1.1.ps1
@@ -1,13 +1,20 @@
 [CmdletBinding()]
-param()
+param(
+    [string]$ShowOdd = "true"  # accept string
+)
 
-# Simple array
+# Convert the string $ShowOdd to boolean:
+# Uses a regex to match So it matches strings like "true", "TRUE",
+# or "1" exactly. If it matches, $ShowOddBool = $true; otherwise $false.
+$ShowOddBool = if ($ShowOdd -match '^(?i)true|1$') { $true } else { $false }
+
 $numbers = 1..5
-
 for ($i = 0; $i -lt $numbers.Length; $i++) {
     if ($numbers[$i] % 2 -eq 0) {
         Write-Output "$($numbers[$i]) is even"
-    } else {
+    } elseif ($ShowOddBool) {
         Write-Output "$($numbers[$i]) is odd"
     }
 }
+
+
